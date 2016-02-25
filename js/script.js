2639074af0d81a041c8ca309c5301be329cfa62a
Jessica Fewick <J.Fenwick@wlv.ac.uk>
2016-02-25 22:15:24 +0000

    Start of the WebGl script

---
+++ js/script.js
@@ -0,0 +1,22 @@
+window.onload = init;
+
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var VIEW_ANGLE = 45;
+var NEAR_ASPECT_RATIO = WIDTH/HEIGHT;
+var NEAR_CLIPPING_PLANE = 0.1;
+var FAR_CLIPPING_PLANE = 10000;
+
+var renderer;
+var scene;
+var camera;
+
+var sphere;
+
+function init(){
+
+    renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(WIDTH, HEIGHT);
+}
