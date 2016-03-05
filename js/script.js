5c2ee5b28889fb4eccb8e2b60984ed45ecae9f48
ping19 <pingsam@hotmail.co.uk>
2016-03-05 21:04:39 +0000

    rename three.js to minified version

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var ASPECT_RATIO = WIDTH / HEIGHT;
 var NEAR_CLIPPING_PLANE = 0.1;
 var FAR_CLIPPING_PLANE = 10000;
 
-//declare variables for three.js
+//declare variables for three.min.js
 var renderer;
 var scene;
 var camera;
@@ -25,7 +25,7 @@ var clock = new THREE.Clock();
 var mouseOverCanvas
 var mouseDown
 
-//store three.js  controls
+//store three.min.js  controls
 var controls;
 
 var seaMesh;
